fd7128614750717576e79e1df5100a59393652c5
MarketKonec <80887086+MarketKonec@users.noreply.github.com>
2022-02-16 14:36:47 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -22,3 +22,19 @@ def plot_model_evaluation(hist):
     plt.plot(x,hist.history['val_loss'],label = 'val_loss')
     plt.legend()
     plt.show()
+
+import tensorflow as tf
+import datetime
+
+def create_tensorboard_callback(dir_name, model_name):
+  '''
+  Creates callback to save model results to dir_name.
+
+  Returns:
+  Callback
+  '''
+  log_dir = dir_name + '/' + model_name + datetime.datetime.now().strftime("_%Y%m%d-%H%M%S")
+  tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir = log_dir)
+  print(f'Saving tensorboard logfiles to {log_dir}')
+  return tensorboard_callback
+    
