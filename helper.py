4659c811ea2c0ca823851fa80889d3a1bb33bb0b
MarketKonec <80887086+MarketKonec@users.noreply.github.com>
2022-02-21 16:49:22 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -69,12 +69,16 @@ def compare_histories(orignal_hist, new_hist, initial_epochs = 5):
 from sklearn.metrics import confusion_matrix
 import numpy as np
 
-def plot_confusion_matrix(y,y_pred, label_description = None):
+from sklearn.metrics import confusion_matrix
+
+def plot_confusion_matrix(y, y_pred, label_description = None, text_size = 10, figsize = (10,10), label_rotation = 0):
     '''
     Args:
     y: true labels.
     y_pred: predicted labels.
     label_description: label description for confusion matrix plot.
+    text_size: text size.
+    figsize: figure size.
     
     Returns accuracy and confusion matrix plot.
     '''
@@ -84,14 +88,13 @@ def plot_confusion_matrix(y,y_pred, label_description = None):
     cm = confusion_matrix(y, y_pred)
   
     if cm.shape[0] == 2:
-      tn, fn, tp, fp = cm[0,0], cm[1,0], cm[1,1], cm[0,1]
-      precision = tp/(tp+fp)
-      recall = tn/(tn+fn)
-      f1_score = 2*precision*recall/(precision+recall)
-      print(f'Precision (TP / AllP) = {(precision*100):.2f}%\nRecall (TN / AllN)= {(recall*100):.2f}%\nF1 Score = {(f1_score*100):.2f}%\n\n')
+        tn, fn, tp, fp = cm[0,0], cm[1,0], cm[1,1], cm[0,1]
+        precision = tp/(tp+fp)
+        recall = tn/(tn+fn)
+        f1_score = 2*precision*recall/(precision+recall)
+        print(f'Precision (TP / AllP) = {(precision*100):.2f}%\nRecall (TN / AllN)= {(recall*100):.2f}%\nF1 Score = {(f1_score*100):.2f}%\n\n')
 
 #plot cm
-    figsize = (5,5) if cm.shape[0] == 2 else (14,14)
     cm_perc = cm.astype('float')/cm.sum(axis=1)[:,np.newaxis]
     fig, ax = plt.subplots(figsize = figsize)
     cax = ax.matshow(cm, cmap = plt.cm.Blues)
@@ -116,7 +119,8 @@ def plot_confusion_matrix(y,y_pred, label_description = None):
     import itertools
 
     for i,j in itertools.product(range(cm.shape[0]),range(cm.shape[0])):
-      plt.text(j,i, f'{cm[i,j]} ({cm_perc[i,j]*100:.1f}%)',
-               horizontalalignment = 'center',
-               color = 'white' if cm[i,j] > threshold else 'black',
-               size = 10)
+        plt.text(j,i, f'{cm[i,j]} ({cm_perc[i,j]*100:.1f}%)',
+                 horizontalalignment = 'center',
+                 color = 'white' if cm[i,j] > threshold else 'black',
+                 size = text_size)
+    plt.xticks(rotation = label_rotation)
